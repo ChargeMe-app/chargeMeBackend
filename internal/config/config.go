f77ce9488b57c45bdad0bc940b6cde03f7bbe0a4
poorfrombabylon <al06maz2002@gmail.com>
2022-10-21 17:39:18 +0300

    init config

--- internal/config/config.go
+++ internal/config/config.go
@@ -1,1 +1,47 @@
 package config
+
+import (
+	"fmt"
+	"gopkg.in/yaml.v3"
+	"os"
+	"path/filepath"
+)
+
+var cfg Config
+
+type Config struct {
+	Postgres      Database      `yaml:"DB"`
+	YandexStorage YandexStorage `yaml:"YandexStorage"`
+}
+
+type Database struct {
+	Host     string `yaml:"host"`
+	Port     int    `yaml:"port"`
+	User     string `yaml:"user"`
+	Password string `yaml:"password"`
+	DBname   string `yaml:"dbname"`
+}
+
+type YandexStorage struct {
+	AwsAccessKeyId     string `yaml:"aws_access_key_id"`
+	AwsSecretAccessKey string `yaml:"aws_secret_access_key"`
+	Region             string `yaml:"region"`
+}
+
+func InitConfig() (*Config, error) {
+	configPath := "cmd/chargeMe/config.yaml"
+
+	clean := filepath.Clean(configPath)
+
+	file, err := os.Open(clean)
+	if err != nil {
+		return nil, fmt.Errorf("fail to open config file in path \"%s\" with error %w", configPath, err)
+	}
+
+	err = yaml.NewDecoder(file).Decode(&cfg)
+	if err != nil {
+		return nil, fmt.Errorf("fail to parse config %w", err)
+	}
+
+	return &cfg, nil
+}
