ba2058c35b9e1a7dc24f066c527d10f2f9a4dc61
poorfrombabylon <al06maz2002@gmail.com>
2023-01-05 23:14:49 +0500

    [CMDEV-19]: имплементация методов для удаления старых станций My.eCars

--- example/delete_stations/delete.go
+++ example/delete_stations/delete.go
@@ -7,6 +7,8 @@ import (
 	"log"
 	"math"
 	"os/signal"
+	"strconv"
+	"strings"
 	"syscall"
 
 	_ "github.com/lib/pq"
@@ -44,6 +46,135 @@ func getDistanceBetweenPoints(latitudeBase, longitudeBase, latitudeNew, longitud
 	return distance * 1.609344
 }
 
+func deleteSitronicsStations(
+	ctx context.Context,
+	storageRegistry *storage.Storages,
+	integrationRegistry integration.Integration,
+) error {
+	allPlaces, err := storageRegistry.PlaceStorage.GetAllPlaces(ctx)
+	if err != nil {
+		log.Fatal("failed to get all places sitronics", err.Error())
+	}
+
+	sitronicsResp, err := integrationRegistry.SitronicsIntegration.GetAllStations(ctx)
+	if err != nil {
+		log.Fatal("failed to get sitronics places ", err.Error())
+	}
+
+	sitronicsPlaces := sitronicsResp.CPList
+
+	for i := range allPlaces {
+		for j := range sitronicsPlaces {
+
+			latitudeBase := float64(allPlaces[i].GetPlaceLatitude())
+			longitudeBase := float64(allPlaces[i].GetPlaceLongitude())
+
+			latitudeNew := float64(sitronicsPlaces[j].Latitude)
+			longitudeNew := float64(sitronicsPlaces[j].Longitude)
+
+			if getDistanceBetweenPoints(latitudeBase, longitudeBase, latitudeNew, longitudeNew) <= 0.04 {
+				err := storageRegistry.AmenityStorage.DeleteAmenitiesByLocationID(ctx, allPlaces[i].GetPlaceID())
+				if err != nil {
+					fmt.Println("placeID", allPlaces[i].GetPlaceID().String())
+					log.Fatal("failed to delete amenity sitronics ", err.Error())
+				}
+
+				stations, err := storageRegistry.StationStorage.GetStationsByPlaceID(ctx, allPlaces[i].GetPlaceID())
+				if err != nil {
+					fmt.Println("placeID", allPlaces[i].GetPlaceID().String())
+					log.Fatal("failed to get stations sitronics ", err.Error())
+				}
+
+				for l := range stations {
+					err = storageRegistry.ReviewStorage.DeleteReviewsByStationID(ctx, stations[l].GetStationID())
+					if err != nil {
+						fmt.Println("stationID", stations[l].GetStationID().String())
+						log.Fatal("failed to delete review sitronics ", err.Error())
+					}
+				}
+
+				err = storageRegistry.PlaceStorage.DeletePlaceByID(ctx, allPlaces[i].GetPlaceID())
+				if err != nil {
+					fmt.Println("placeID", allPlaces[i].GetPlaceID().String())
+					log.Fatal("failed to delete place sitronics ", err.Error())
+				}
+			}
+
+		}
+	}
+
+	return nil
+}
+
+func deleteMyECarsStations(
+	ctx context.Context,
+	storageRegistry *storage.Storages,
+	integrationRegistry integration.Integration,
+) error {
+	allPlaces, err := storageRegistry.PlaceStorage.GetAllPlaces(ctx)
+	if err != nil {
+		log.Fatal("failed to get all places my.eCars", err.Error())
+	}
+
+	myECarsResp, err := integrationRegistry.MyECarsIntegration.GetAllStations(ctx)
+	if err != nil {
+		log.Fatal("failed to get my.eCars places ", err.Error())
+	}
+
+	myECarsPlaces := myECarsResp.Evse
+
+	for i := range allPlaces {
+		for j := range myECarsPlaces {
+
+			coordsNew := strings.Split(myECarsPlaces[j].Location, ",")
+
+			latitudeBase := float64(allPlaces[i].GetPlaceLatitude())
+			longitudeBase := float64(allPlaces[i].GetPlaceLongitude())
+
+			latitudeNew, err := strconv.ParseFloat(coordsNew[0], 64)
+			if err != nil {
+				log.Fatal("failed to get my.eCars latitude ", err.Error())
+			}
+
+			longitudeNew, err := strconv.ParseFloat(coordsNew[1], 64)
+			if err != nil {
+				log.Fatal("failed to get my.eCars longitude ", err.Error())
+			}
+
+			if getDistanceBetweenPoints(latitudeBase, longitudeBase, latitudeNew, longitudeNew) <= 0.04 {
+				err := storageRegistry.AmenityStorage.DeleteAmenitiesByLocationID(ctx, allPlaces[i].GetPlaceID())
+				if err != nil {
+					fmt.Println("placeID", allPlaces[i].GetPlaceID().String())
+					log.Fatal("failed to delete amenity my.eCars ", err.Error())
+				}
+
+				stations, err := storageRegistry.StationStorage.GetStationsByPlaceID(ctx, allPlaces[i].GetPlaceID())
+				if err != nil {
+					fmt.Println("placeID", allPlaces[i].GetPlaceID().String())
+					log.Fatal("failed to get stations my.eCars ", err.Error())
+				}
+
+				for l := range stations {
+					err = storageRegistry.ReviewStorage.DeleteReviewsByStationID(ctx, stations[l].GetStationID())
+					if err != nil {
+						fmt.Println("stationID", stations[l].GetStationID().String())
+						log.Fatal("failed to delete review my.eCars ", err.Error())
+					}
+				}
+
+				err = storageRegistry.PlaceStorage.DeletePlaceByID(ctx, allPlaces[i].GetPlaceID())
+				if err != nil {
+					fmt.Println("placeID", allPlaces[i].GetPlaceID().String())
+					log.Fatal("failed to delete place my.eCars ", err.Error())
+				}
+			}
+
+		}
+	}
+
+	return nil
+}
+
 func main() {
 	ctx, cancel := signal.NotifyContext(
 		context.Background(),
@@ -76,55 +207,13 @@ func main() {
 
 	integrationRegistry := integration.NewIntegrationRegistry(cfg)
 
-	allPlaces, err := storageRegistry.PlaceStorage.GetAllPlaces(ctx)
+	err = deleteSitronicsStations(ctx, storageRegistry, integrationRegistry)
 	if err != nil {
-		log.Fatal("failed to get all places ", err.Error())
+		fmt.Println("deleteSitronicsStations error: ", err.Error())
 	}
 
-	sitronicsResp, err := integrationRegistry.SitronicsIntegration.GetAllStations(ctx)
+	err = deleteMyECarsStations(ctx, storageRegistry, integrationRegistry)
 	if err != nil {
-		log.Fatal("failed to get sitronics places ", err.Error())
-	}
-
-	sitronicsPlaces := sitronicsResp.CPList
-
-	for i := range allPlaces {
-		for j := range sitronicsPlaces {
-
-			latitudeBase := float64(allPlaces[i].GetPlaceLatitude())
-			longitudeBase := float64(allPlaces[i].GetPlaceLongitude())
-
-			latitudeNew := float64(sitronicsPlaces[j].Latitude)
-			longitudeNew := float64(sitronicsPlaces[j].Longitude)
-
-			if getDistanceBetweenPoints(latitudeBase, longitudeBase, latitudeNew, longitudeNew) <= 0.02 {
-				err := storageRegistry.AmenityStorage.DeleteAmenitiesByLocationID(ctx, allPlaces[i].GetPlaceID())
-				if err != nil {
-					fmt.Println("placeID", allPlaces[i].GetPlaceID().String())
-					log.Fatal("failed to delete amenity ", err.Error())
-				}
-
-				stations, err := storageRegistry.StationStorage.GetStationsByPlaceID(ctx, allPlaces[i].GetPlaceID())
-				if err != nil {
-					fmt.Println("placeID", allPlaces[i].GetPlaceID().String())
-					log.Fatal("failed to get stations ", err.Error())
-				}
-
-				for l := range stations {
-					err = storageRegistry.ReviewStorage.DeleteReviewsByStationID(ctx, stations[l].GetStationID())
-					if err != nil {
-						fmt.Println("stationID", stations[l].GetStationID().String())
-						log.Fatal("failed to delete review ", err.Error())
-					}
-				}
-
-				err = storageRegistry.PlaceStorage.DeletePlaceByID(ctx, allPlaces[i].GetPlaceID())
-				if err != nil {
-					fmt.Println("placeID", allPlaces[i].GetPlaceID().String())
-					log.Fatal("failed to delete place ", err.Error())
-				}
-			}
-
-		}
+		fmt.Println("deleteMyECarsStations error: ", err.Error())
 	}
 }
